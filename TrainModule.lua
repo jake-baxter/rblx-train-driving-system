a838df8b7a20f2466175253fa794f0cfb0a344da
Jake Baxter <77267633+jake-baxter@users.noreply.github.com>
2021-10-25 10:36:18 +0100

    Create TrainModule.lua
    Added basic documentation

---
+++ TrainModule.lua
@@ -0,0 +1,16 @@
+--[[
+	// **READ-ONLY**
+	// FileName: TrainModule.lua
+	// Written by: Jake Baxter
+	// Version 1.0
+	// Description: An API for train control in roblox.
+
+	// Contributors:
+		
+	
+
+	// Required by:
+		Train Scripts
+
+	// Note: for documentation see *inserting devforum link here*
+--]]
