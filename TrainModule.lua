9c74689a6e9d9f724b659e68d8ef38c01f37f3b7
Jake Baxter <77267633+jake-baxter@users.noreply.github.com>
2021-10-29 20:18:52 +0100

    added 4 stuff
    right lets gooooo

--- TrainModule.lua
+++ TrainModule.lua
@@ -39,7 +39,7 @@ function TrainModule.new(trainModel, data)
 		error("Throttle must be greater to or 1!")
 	end
 	classSelf.throttle = data["throttle"]
-	
+
 
 	assert(data["brake"], "Include Brake Value")
 	if not (type(data["brake"]) == "int") then
@@ -49,7 +49,7 @@ function TrainModule.new(trainModel, data)
 		error("Brake must be greater to or 1!")
 	end
 	classSelf.brake = data["brake"]
-		
+
 
 	assert(data["throttlePower"], "Include Throttle Power Value")
 	if not (type(data["throttlePower"]) == "int" or type(data["throttlePower"]) == "number" or type(data["throttlePower"]) == "float") then
@@ -59,7 +59,7 @@ function TrainModule.new(trainModel, data)
 		error("Throttle Power must be greater than 0!")
 	end
 	classSelf.throttlePower = data["throttlePower"]
-			
+
 
 	assert(data["brakePower"], "Include Throttle Power Value")
 	if not (type(data["brakePower"]) == "int" or type(data["brakePower"]) == "number" or type(data["brakePower"]) == "float") then
@@ -69,6 +69,46 @@ function TrainModule.new(trainModel, data)
 		error("Brake Power must be greater than 0!")
 	end
 	classSelf.brakePower = data["brakePower"]
+
+
+	assert(data["throttleFullTime"], "Include Throttle Up Time Value")
+	if not (type(data["throttleFullTime"]) == "int" or type(data["throttleFullTime"]) == "number" or type(data["throttleFullTime"]) == "float") then
+		error("Throttle Up Time  must be a number / int / float!")
+	end
+	if data["throttleFullTime"] < 0 then
+		error("Throttle Up Time must be greater than 0!")
+	end
+	classSelf.throttleFullTime = data["throttleFullTime"]
+
+
+	assert(data["throttleIdleTime"], "Include Throttle Down Time Value")
+	if not (type(data["throttleIdleTime"]) == "int" or type(data["throttleIdleTime"]) == "number" or type(data["throttleIdleTime"]) == "float") then
+		error("Throttle Down Time  must be a number / int / float!")
+	end
+	if data["throttleIdleTime"] < 0 then
+		error("Throttle Down Time must be greater than 0!")
+	end
+	classSelf.throttleIdleTime = data["throttleIdleTime"]
+
+
+	assert(data["brakeFullTime"], "Include Brake Up Time Value")
+	if not (type(data["brakeFullTime"]) == "int" or type(data["brakeFullTime"]) == "number" or type(data["brakeFullTime"]) == "float") then
+		error("Brake Up Time  must be a number / int / float!")
+	end
+	if data["brakeFullTime"] < 0 then
+		error("Brake Up Time must be greater than 0!")
+	end
+	classSelf.brakeFullTime = data["brakeFullTime"]
+
+
+	assert(data["brakeIdleTime"], "Brake Throttle Down Time Value")
+	if not (type(data["brakeIdleTime"]) == "int" or type(data["brakeIdleTime"]) == "number" or type(data["brakeIdleTime"]) == "float") then
+		error("Brake Down Time  must be a number / int / float!")
+	end
+	if data["brakeIdleTime"] < 0 then
+		error("Brake Down Time must be greater than 0!")
+	end
+	classSelf.brakeIdleTime = data["brakeIdleTime"]
 end
 
 return TrainModule
