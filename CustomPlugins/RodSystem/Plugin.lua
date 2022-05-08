23d339fd788a9ad78c01fad8fbbf3218e038b6d5
Jake Baxter <77267633+jake-baxter@users.noreply.github.com>
2022-05-08 20:40:51 +0100

    v0.0.0-alpha.3
    Rod scripts

--- CustomPlugins/RodSystem/Plugin.lua
+++ CustomPlugins/RodSystem/Plugin.lua
@@ -1,8 +1,8 @@
 --[[
 	// FileName: RodSystem.lua
 	// Written by: Jake Baxter
-	// Version: v0.0.0-alpha.2
-	// Description: Prepares NWSpacek Rod System for driving system.
+	// Version: v0.0.0-alpha.3
+	// Description: Prepares NWSpacek Rod System for driving system. To learn how to use this check "rod values.txt" in the github.
 
 	// Contributors:
         
@@ -12,6 +12,22 @@ local Plugin = {}
 Plugin.Name = "RodSystem"
 Plugin.Version = {0, 0, 0} --//Change the first two numbers to the appropiate driving, the last one is optional for plugin identification
 Plugin.__index = Plugin
+
+local HttpService = game:GetService("HttpService")
+
+local function addMyLocalScript(player, LocalScript)
+	local localScript = LocalScript:Clone()
+	local playerGui = player:WaitForChild("PlayerGui")
+	local trainGui = player:FindFirstChild("PersistentTrainGui")
+	if not trainGui then
+		trainGui = Instance.new("ScreenGui")
+		trainGui.Name = "PersistentTrainGui"
+		trainGui.ResetOnSpawn = false
+		trainGui.Parent = playerGui
+	end
+	localScript.Parent = trainGui
+    return localScript
+end
 
 function Plugin.init(superiorSelf, EventListener)
 	local classSelf = {}
@@ -23,14 +39,76 @@ function Plugin.init(superiorSelf, EventListener)
         return Plugin
     end
 
+
+    classSelf.ActiveLocos = {}
 	classSelf.superiorSelf = superiorSelf
     classSelf.RegisteredFunctions = {}
 
 
+    local AddLocoFunc =  superiorSelf:GetServerEventConnection():connect(function(ModuleName, SentData)
+        if not (ModuleName == "RodSystem") then
+            return false
+        end
+
+        if not(type(SentData) == "table") then
+            warn("Incorrect Syntax for rods!")
+            return false
+        end
+
+        if not (SentData["action"] == "AddLoco") then
+            return false
+        end
+
+        if not (typeof(SentData["Model"]) == "Instance") then
+		    warn("Set Instance in rods 'Model'!")
+            return false
+	    end
 
 
+        if not (type(SentData["RodValues"]) == "table") then
+		    warn("Set Table As RodValues!")
+            return false
+	    end
+
+        if not (SentData["Model"]["Name"] == "Moving Parts") then
+		    warn("The model of what we're talking about must be called 'Moving Parts' (Rod Script)")
+            return false
+	    end
+
+        if classSelf.ActiveLocos[SentData["Model"]] then
+            return false
+        end
+
+        classSelf.ActiveLocos[SentData["Model"]] = SentData
+
+        for _,InGPlayers in pairs(game.Players:GetChildren()) do
+            local ClonedLocalScript = addMyLocalScript(InGPlayers, LocalScriptCopy)
+            if ClonedLocalScript then
+                ClonedLocalScript:SetAttribute("Data", HttpService:JSONEncode(SentData["RodValues"]))
+                local LocoValue = Instance.new("ObjectValue")
+                LocoValue.Name = "loco"
+                LocoValue.Value = SentData["Model"]
+            end
+        end
 
 
+    end)
+
+    classSelf.RegisteredFunctions[AddLocoFunc] = true
+
+
+    local PlayerJoined = game.Players.PlayerAdded:connect(function(plr)
+        for MovingPartModels, MovingPartsData in pairs(classSelf.ActiveLocos) do
+            local ClonedLocalScript = addMyLocalScript(plr, LocalScriptCopy)
+            if ClonedLocalScript then
+                ClonedLocalScript:SetAttribute("Data", HttpService:JSONEncode(MovingPartsData["RodValues"]))
+                local LocoValue = Instance.new("ObjectValue")
+                LocoValue.Name = "loco"
+                LocoValue.Value = MovingPartModels
+            end
+        end
+    end)
+    classSelf.RegisteredFunctions[PlayerJoined] = true
 
 
     
@@ -43,7 +121,7 @@ function Plugin:OnDisable()
         return false
     end
 
-    for _, functionsToDisable in pairs(self.RegisteredFunctions) do
+    for functionsToDisable, _ in pairs(self.RegisteredFunctions) do
         if (typeof(functionsToDisable) == "function") then
             functionsToDisable:Disconnect()
         end
